7b07913c48f32bcc75a924a221e80f8080988978
kennyaliyev <kenny.aliyev@gmail.com>
2025-12-14 19:38:05 +0000

    Add 6-raise_exception_msg.py: raise NameError with message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError with the given message.
+    Args:
+        message (str): Message to include in the exception.
+    """
+    raise NameError(message)
