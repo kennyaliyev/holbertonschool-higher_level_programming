5107afe5dd3e609808e5520f147e7f2fb97cc61d
kennyaliyev <kenny.aliyev@gmail.com>
2025-12-14 19:33:31 +0000

    Add 4-list_division.py: element-wise division with error handling

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Divides elements of two lists element-wise.
+    Args:
+        my_list_1 (list): First list.
+        my_list_2 (list): Second list.
+        list_length (int): Length of result list.
+    Returns:
+        list: New list of divisions (length = list_length).
+    """
+    new_list = []
+    for i in range(list_length):
+        try:
+            val1 = my_list_1[i]
+            val2 = my_list_2[i]
+            result = val1 / val2
+            new_list.append(result)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+    return new_list
