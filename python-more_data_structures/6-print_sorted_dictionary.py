c1a6dc2731927168a3bae945858f76d36ce2ae26
kennyaliyev <kenny.aliyev@gmail.com>
2025-12-14 19:11:46 +0000

    Add 6-print_sorted_dictionary.py: print dict by sorted keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by sorted keys (alphabetical order, case-sensitive).
+
+    Args:
+        a_dictionary (dict): The dictionary to print.
+    
+    Note:
+        - Only top-level keys are sorted and printed.
+        - Keys are assumed to be strings.
+        - Values can be of any type.
+    """
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
