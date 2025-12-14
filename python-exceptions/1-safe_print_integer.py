b60341ff0fdcc8f8b22f0d754f5f5374aa20d154
kennyaliyev <kenny.aliyev@gmail.com>
2025-12-14 19:28:55 +0000

    Add 1-safe_print_integer.py: safely print integer with {:d}

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Prints an integer using '{:d}'.format().
+    Args:
+        value: Any type.
+    Returns:
+        bool: True if printed as integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
