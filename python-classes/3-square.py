f648f3a9f8cf973ad581117c9688e29e2e8279df
kennyaliyev <kenny.aliyev@gmail.com>
2025-12-21 18:28:11 +0000

    Add 3-square.py: Square with area method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -18,8 +18,9 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        
         """Computes and returns the current area of the square.
+
+        
         Returns:
             int: Area = size * size.
         """
