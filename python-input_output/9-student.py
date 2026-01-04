95b7687feaad0a0cf828050a47e6032583e85993
kennyaliyev <kenny.aliyev@gmail.com>
2026-01-04 18:39:44 +0000

    Add 9-student.py: Student class with to_json method

---
+++ python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a Student class with JSON serialization support."""
+
+
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary of the student's attributes.
+        """
+        return self.__dict__
