8a463cf0e704e741d1f426d580fd5f37d9bea524
kennyaliyev <kenny.aliyev@gmail.com>
2026-01-04 18:28:54 +0000

    Add 2-append_write.py: append string to file and return count

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Appends a string to a text file (UTF-8) and returns number of chars added."""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to a text file (UTF-8) and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file to append to. Defaults to "".
+        text (str): The text to append. Defaults to "".
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
