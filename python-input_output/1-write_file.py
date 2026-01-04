18d1635cd5cc9c7ef1528a2aa11d63f04ff90885
kennyaliyev <kenny.aliyev@gmail.com>
2026-01-04 18:24:21 +0000

    Add 1-write_file.py: write string to file and return count

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Writes a string to a text file (UTF-8) and returns character count."""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8) and returns the number of characters written.
+
+    Args:
+        filename (str): The name of the file to write to. Defaults to "".
+        text (str): The text to write. Defaults to "".
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
