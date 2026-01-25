2d62c9cccff0a7cc2a801023399e090f6bc934e0
kennyaliyev <kenny.aliyev@gmail.com>
2026-01-25 19:30:51 +0000

    Fix pycodestyle errors in 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,8 +19,9 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-    
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
     states = cursor.fetchall()
     for state in states:
         print(state)
