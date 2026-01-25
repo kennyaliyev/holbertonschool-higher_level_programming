609b45d16cf1f1a7424b58c148f140efbd54f2e5
kennyaliyev <kenny.aliyev@gmail.com>
2026-01-25 19:43:48 +0000

    Fix 14-model_city_fetch_by_state.py: use explicit join query

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""Fetch all City objects and print with their State name."""
+"""Fetch cities with their state names."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 from model_city import City
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            username, password, database
-        ),
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, passwd, db),
         pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query cities joined with states, ordered by city id
-    cities = session.query(City).join(State).order_by(City.id).all()
+    # Join City and State, order by city id
+    cities = session.query(City, State).join(State).order_by(City.id).all()
 
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    for city, state in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""City class definition for SQLAlchemy ORM."""
+"""City class definition."""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, ForeignKey, Integer, String
 from model_state import Base
 
 
 class City(Base):
-    """City class that links to the MySQL table 'cities'."""
+    """City class linked to 'cities' table."""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
