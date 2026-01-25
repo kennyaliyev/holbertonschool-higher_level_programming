6977f221f21e4f31548226977b90ec1e0679b08f
kennyaliyev <kenny.aliyev@gmail.com>
2026-01-25 19:28:39 +0000

    Fix 1-filter_states.py: enforce uppercase N with BINARY

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""List all states with name starting with 'N' from hbtn_0e_0_usa."""
+"""List all states with name starting with 'N' (uppercase) from hbtn_0e_0_usa."""
 
 import sys
 import MySQLdb
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    
     states = cursor.fetchall()
     for state in states:
         print(state)
